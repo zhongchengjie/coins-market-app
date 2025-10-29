4a5214d6fe5a1d0de98d666bd46502458c16df5c
zhongchengjie <465152138@qq.com>
2025-10-30 07:29:53 +0800

    feat: 排序功能&优化

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -3,11 +3,11 @@ import type { Knex } from "knex";
 export const dbConf: Knex.Config  = {
   client: 'mysql',
   connection: {
-    host: process.env.DB_HOST || "sh-cynosdbmysql-grp-jrm87l8a.sql.tencentcdb.com",
-    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 24397,
-    user: process.env.DB_USER || "w33_2025",
-    password: process.env.DB_PASS || "258000Qwer@",
-    database: process.env.DB_NAME || "coin_market_dev",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3006,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     timezone: 'UTC'
   }
 };
